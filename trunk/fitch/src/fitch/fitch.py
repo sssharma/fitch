12e44e41fb566a5e00003df4cc8ed7cea9384e90
jak.plopelor@gmail.com <jak.plopelor@gmail.com@76a4a9ed-01a9-ba77-4278-4ce6c70b14eb>
2011-06-27 18:45:33 +0000

    fitch for a whole sequence in class.

--- trunk/fitch/src/fitch/fitch.py
+++ trunk/fitch/src/fitch/fitch.py
@@ -9,7 +9,7 @@ class fitch_tree(tree):
 		
 		self.gtr = tr
 		self.possible_r_trees = []
-		self.possible_trees = set()
+		self.possible_trees = []
 		self.len = len(self.gtr.get_leaf().r)
 		
 		for i in range(self.len):
@@ -25,14 +25,34 @@ class fitch_tree(tree):
 			self.R(index_tree)
 			self.possible_r_trees.append(set([index_tree]))
 			self.r(index_tree.get_root(),index_tree, self.possible_r_trees[i])
+		self.get_possible_trees()
 		
-	def get_possible_trees(self, list_of_sets_of_trees):
+	def get_possible_trees(self):
 		
-		if not list_of_sets_of_trees: return 
+		self.names = []
+		for node in self.gtr.nodes:
+			self.names.append(node.name)
 		
-		for tree in list_of_sets_of_trees[0]:
+		dic = {}
+		for name in self.names:
+			dic[name] = ''
+		
+		self.possible_trees.append(dic)
+	
+		
+		for i in range(self.len):
 			
-			self.possible_trees = self.possible_trees.union( self.get_possible_trees(list_of_sets_of_trees[1:]))
+			new_list = []
+			for place in self.possible_trees:
+				
+				for itree in self.possible_r_trees[i]:
+				
+					new_dic = dict(place)
+					for node in itree.nodes:
+						new_dic[node.name] += node.r
+					new_list.append(new_dic)
+					 
+			self.possible_trees = new_list
 	
 	def R(self, tr):
 		
@@ -126,69 +146,10 @@ if __name__ == '__main__':
 
 	tr.add_edge(v3, v6)
 	tr.add_edge(v3, v7)
-	
-	tr2 = genetic_tree()
-
-	v1 = specie(name = 'v1')
-	v2 = specie(name = 'v2')
-	v3 = specie(name = 'v3')
-	v4 = specie('A', name = 'v4')
-	v5 = specie('A', name = 'v5')
-	v6 = specie('C', name = 'v6')
-	v7 = specie('G', name = 'v7')
-
-	tr2.root = v1
-
-	tr2.add_edge(v1, v2)
-	tr2.add_edge(v1, v3)
-
-	tr2.add_edge(v2, v4)
-	tr2.add_edge(v2, v5)
-
-	tr2.add_edge(v3, v6)
-	tr2.add_edge(v3, v7)
 
 	fitch = fitch_tree(tr)
 	
-	fitch.R(tr2)
-	
-	s = set([tr2])
-	fitch.r(tr2.get_root(), tr2, s)
-	'''
-	for tree in s:
-		print tree
 
-
-	for i in range(2):
-		print i
-		for trees in fitch.possible_r_trees:
-			for tree in trees:	
-				print tree
-
-	'''
-	
-	list_of_options = [{'v1':'','v2':'','v3':'','v4':'','v5':'','v6':'','v7':''}]
-	
-	for i in range(2):
-		
-		new_list = []
-		for place in list_of_options:
-			
-			for itree in fitch.possible_r_trees[i]:
-			
-				new_dic = dict(place)
-				for node in itree.nodes:
-					new_dic[node.name] += node.r
-				new_list.append(new_dic)
-				 
-		list_of_options = new_list
-			
-	for list in list_of_options:
+	for list in fitch.possible_trees:
 		print list
-				
-				
-				
-				
-				
-				
 				
